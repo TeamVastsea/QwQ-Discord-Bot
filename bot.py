909c97cef88381a326998a3f5cf3e98152ed3cb0
JianyueLab <valor@jianyuelab.org>
2024-10-04 20:23:13 +0900

    Init

---
+++ bot.py
@@ -0,0 +1,43 @@
+# imports
+import os
+import discord
+from discord import app_commands
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# load env file
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix='!', intents=intents)
+
+# start bot
+@client.event
+async def on_ready():
+    try:
+        synced = await client.tree.sync()
+        print("Bot is ready!")
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+
+# cat snowball
+@client.tree.command(name='cat', description="cat SnowBall")
+async def cat(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        """
+        好无聊逗逗雪球吧
+        嘬嘬嘬𐃆 ˒˒ ͏                               
+͏
+͏
+͏
+͏                              ╱|、
+                            (˚ˎ 。7 
+                            |、˜ 〵 
+                            じしˍ,)ノ
+        """
+    )
+
+client.run(DISCORD_TOKEN)
